57082cd46675b150f8c9abcb2e7100f6642ea897
cagrigsby <162646448+cagrigsby@users.noreply.github.com>
2024-09-12 00:36:55 -0500

    Update draft.sh

--- draft.sh
+++ draft.sh
@@ -48,7 +48,7 @@ for entry in "${repositories[@]}"; do
 done
 
 
-
+# This section moves existing kali binaries to the appropriate server
 cp /usr/share/windows-resources/powersploit/Privesc/PowerUp.ps1 ~/server;
 cp /usr/share/windows-resources/binaries/nc.exe ~/server/nc.exe;
 cp /usr/share/windows-resources/mimikatz/x64/mimikatz.exe ~/server/mimikatz64.exe;

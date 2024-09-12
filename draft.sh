82c988bf04eae80501f1b156efb1ce7352653e25
cagrigsby <162646448+cagrigsby@users.noreply.github.com>
2024-09-12 00:29:32 -0500

    Create draft.sh

---
+++ draft.sh
@@ -0,0 +1,6 @@
+#This is a working draft for creating a folder useful binaries to transfer to a target
+
+mkdir ~/server;
+cp /usr/share/windows-resources/powersploit/Privesc/PowerUp.ps1 ~/server;
+cp /usr/share/windows-resources/binaries/nc.exe ~/server/nc.exe;
+cp /usr/share/windows-resources/mimikatz/x64/mimikatz.exe ~/server/mimikatz64.exe;

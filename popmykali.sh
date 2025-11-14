d0aae7f9e7007a36ef3470737275099b842410f1
pentestpop <162646448+pentestpop@users.noreply.github.com>
2025-11-14 13:31:13 -0600

    Update popmykali.sh

--- popmykali.sh
+++ popmykali.sh
@@ -114,8 +114,92 @@ read -p "Enter your choice: " user_input;
 # Convert input to lowercase and check if it's 'y' or 'yes'
 if [[ "$user_input" =~ ^[Yy]([Ee][Ss])?$ ]]; then
     echo "Running the optimization script..."
-    # Replace 'optimize_script.sh' with the actual path to the script you want to run
-    bash /opt/PopMyKali/poptimize.sh
+    
+    # PopScripts
+    git clone https://github.com/pentestpop/PopScripts.git /opt/PopScripts
+    # PopScripts symbolic links
+    chmod +x /opt/PopScripts/link.sh && bash /opt/PopScripts/link.sh;
+    
+    # append custom aliases etc. to .zshrc
+    cat /opt/PopMyKali/zshrc_additions.txt >> ~/.zshrc;
+    
+    # copy orville.jpg to ~/Pictures/. so you have a test photo
+    cp /opt/PopMyKali/samples/Sample.jpg ~/Pictures/Sample.jpg;
+    cp /opt/PopMyKali/samples/Sample.png ~/Pictures/Sample.png;
+    cp /opt/PopMyKali/samples/orville.jpg ~/Pictures/orville.jpg
+    
+    # create a folder with useful scripts to run a server from
+    bash /opt/PopMyKali/servefolder.sh;
+    
+    # create a symlink for verybasicnamp/vbnmap.sh
+    chmod +x /opt/verybasicenum/vbnmap.sh;
+    ln -s /opt/verybasicenum/vbnmap.sh /usr/local/bin/vbnmap;
+    
+    # customize terminator
+    pip install requests;
+    mkdir -p $HOME/.config/terminator/plugins;
+    wget https://git.io/v5Zww -O $HOME"/.config/terminator/plugins/terminator-themes.py";
+    cp /opt/PopMyKali/terminatorconfig ~/.config/terminator/config;
+    echo " Solarized Dark - Patched is a decent template for terminator";
+    
+    # To add a white text on blue background for your terminal, add this to .zshrc:
+    echo "PS1='%{\$(tput setab 4)\$(tput setaf 7)%}%(#.\$USER.\$USER)@%m%{\$(tput sgr0)%}-%{\$(tput setaf 4)%}-%{\$(tput sgr0)%}[%~]%{\$(tput sgr0)%}\$ '" >> ~/.zshrc;
+    
+    # ASCII art
+    octopascii="
+                                                                                                           
+                                                    %%@@@@@@%                                                
+                                                 @@@@@@@@@@@@@@@-                                            
+                                             -+%@@@@@@@@@@@@@@@@@@+-                                         
+                            %@@@@@         %@@@@@@@@@@@@@@@@@@@@@@@@@%         @@@@@%                        
+                          @@@      @@      @%:@@@               @@@:@%      +@      @@@=                     
+                         @@%         .     =@@@@                 @@@@=      .        :@@                     
+                         @@@                . #@                 @% .                %@@=                    
+                         @@@@               . @*                 #@ .               @@@@                     
+                          @@@@               @@@                 @@@               @@@@                      
+                 @@@@@@@@@@@@@@               @%                @@@               @@@@@@@@@@@@@@             
+              @@@@@@@@@@@@@@@@@@@             @@.       .@@@@   %@ %            @@@@@@@@@@@@@@@@@@@.         
+            @@@@@@@    *@@@@@@@@@@@=           @@@.           @@@:           .@@@@@@@@@@@*    #@@@@@@        
+           @@@@-           @@@@@@@@@@@          :@@@@@@@@@@@@@@@           .@@@@@@@@@@            @@@@.      
+          @@@@             @@@@@@@@@@@@@         @@@@@@@@@@@@@@@         :@@@@@@@@@@@@             @@@@      
+          @@@.             @@@@@ +@@@@@@@@:    @@@@@@@@@@@@@@@@@@@    .@@@@@@@@@ @@@@@              @@@      
+          @@@=             @@@@@@  %@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@  @@@@@@              @@@      
+           @@@              @@@@@@@   @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@   @@@@@@@              @@@       
+            @@=              @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@              .@@        
+             @@         #@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@         @@=        
+             %@      =@@@@@@@@@@@@@@@@@@@@@@= @@@@@@@@@@@@@@@@@@@@@ :@@@@@@@@@@@@@@@@@@@@@@%      @@         
+              @     @@@@@*   -@@@@@@@@@.    @@@@@@@@@@@@@@@@@@@@@@@@@.    @@@@@@@@@=   -@@@@@     @          
+             *@    @@@@          @@@@@@@@@@@@@@@@@-@@@@@@@@@@@.@@@@@@@@@@@@@@@@@          @@@@    @@         
+            %@    @@@@             -@@@@@@@@@@@@:  @@@@@@@@@@@*  @@@@@@@@@@@@#             +@@@    :@        
+                  @@@                  :@@@@:     @@@@@@%@@@@@@     :%@@@*                  @@@              
+                  @@@.                          :@@@@@@@ %@@@@@@%                           @@@              
+                   @@@                        -@@@@@@@%   .@@@@@@@%                        @@@               
+                    @@@                    =@@@@@@@@@       @@@@@@@@@%                    @@@                
+                     @@@                @@@@@@@@@@@           @@@@@@@@@@@                @@@                 
+                       @@           +@@@@@@@@@@@                 *@@@@@@@@@@@           @@:                  
+                        @@        @@@@@@@@@@                         *@@@@@@@@@:       @@                    
+                         @#     @@@@@@@@                                 *@@@@@@@.    .@                     
+                         @#   .@@@@@:                                       .@@@@@+   .@                     
+                         @    @@@@@                                           .@@@@    @                     
+                        @    :@@@@                                             +@@@%    @                    
+                             :@@@.                                              @@@@                         
+                              @@@@                                             *@@@                          
+                              +@@@.              @.            @               @@@@                          
+                               -@@@@           @@               :@-          %@@@@                           
+                                 @@@@@@.   @@@%                   -@@@    @@@@@@                             
+                                    *@@@@@@-                          @@@@@@@                                
+                                                                                                             
+                                                                                                             
+                                                                                                             
+                                                                                                            
+    "
+    # Split the ASCII art into lines and iterate over them
+    IFS=$'\n'       # Set the Internal Field Separator to newline
+    for line in $octopascii; do
+        echo "$line"
+        sleep 0.2    # Adjust the sleep duration (in seconds) as needed
+    done
+
 else
     echo "Skipping poptimization."
 fi
@@ -127,3 +211,4 @@ gunzip /usr/share/wordlists/rockyou.txt.gz 2>/dev/null
 
 echo "Create a kerbrute symbolic link by running ' sudo cp /opt/kerbrute/dist/kerbrute\$ /usr/local/bin/kerbrute";
 echo "Don't forget to run sudo updatedb"
+echo "Don't forget to sync firefox with your +kali account!";
